92b20e3d1d28d3662ace5f5332b8dba4fc0aa7bf
musekorea <musekorea@hanmail.net>
2022-04-11 22:08:01 +0800

    mouse info

---
+++ 05_mouse_doubleClick.py
@@ -0,0 +1,5 @@
+import pyautogui
+
+pyautogui.moveTo(379,333, duration=0.5)
+pyautogui.doubleClick()
+
---
+++ 06_mouse_info.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+#pyautogui.mouseInfo()
+
+pyautogui.PAUSE = 1
+
+pyautogui.moveTo(100,100)
+for i in range(10):
+  pyautogui.move(100,100)
+  
